2c1c87f2269f8aba9b88c1055dac7d952ac948d2
Marcos Holgado <marcos.holgadomolina@sky.uk>
2018-11-06 13:59:31 +0000

    Write a simple espresso tests to scroll to the last item on the list

--- app/src/androidTest/java/com/marcosholgado/performancetest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/marcosholgado/performancetest/ExampleInstrumentedTest.kt
@@ -1,24 +1,32 @@
 package com.marcosholgado.performancetest
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.marcosholgado.performancetest", appContext.packageName)
+@LargeTest
+class MyClassTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test fun scrollsThroughList() {
+        val appContext: Context = ApplicationProvider.getApplicationContext()
+
+        for (i in 0..999) {
+            onView(withId(R.id.recyclerView))
+                .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(i))
+        }
     }
 }
