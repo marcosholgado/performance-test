caee046fa5af79a334b464068ce3404be48ba175
Marcos Holgado <marcos.holgadomolina@sky.uk>
2019-01-23 12:52:12 +0000

    Rather than ignoring leak tests, ask specifically to run them by using its own annotation

---
+++ app/src/androidTest/java/com/marcosholgado/performancetest/ninthTest/LeakTest.kt
@@ -0,0 +1,5 @@
+package com.marcosholgado.performancetest.ninthTest
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+annotation class LeakTest
---
+++ app/src/androidTest/java/com/marcosholgado/performancetest/ninthTest/MyOtherLeakRunListener.kt
@@ -0,0 +1,13 @@
+package com.marcosholgado.performancetest.ninthTest
+
+import com.squareup.leakcanary.FailTestOnLeakRunListener
+import org.junit.runner.Description
+
+class MyOtherLeakRunListener: FailTestOnLeakRunListener() {
+    override fun skipLeakDetectionReason(description: Description): String? {
+        return if(description.getAnnotation(LeakTest::class.java) != null)
+            null
+        else
+            "Skip Leak test"
+    }
+}
